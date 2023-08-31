0ab6bdd29eb024cd9e80372f80b23697692e5a18
Anton <60780638+antonio63963@users.noreply.github.com>
2023-08-31 20:37:02 +0300

    fixed bug with uncontrolled value + validation has done

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -4,69 +4,79 @@ import { zodResolver } from '@hookform/resolvers/zod';
 
 import AuthPageLayout from './AuthPageLayout';
 import Database from '../../data/database';
-import { Alert, Box, Card, Dialog, Modal, Typography } from '@mui/material';
+
+import { UseFormReturn, useForm } from 'react-hook-form';
 
 const validationSchema = z.object({
-  name: z.string(),
+  name: z.string().min(2, 'String must contain at list 2 characters'),
   email: z.string().email().transform((val) => val.trim()),
-  password: z.string(),
+  password: z.string().min(6),
+  repeatPassword: z.string().min(6)
+}).superRefine(({ repeatPassword, password }, ctx) => {
+  if (repeatPassword !== password) {
+    ctx.addIssue({
+      code: "custom",
+      message: "The passwords did not match",
+      path: ['repeatPassword']
+    });
+  }
 });
 
 type FormData = {
   name: string;
   email: string;
   password: string;
+  repeatPassword: string;
   isAuthor: boolean;
 }
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-};
 
 const AuthPage: FC = () => {
   const [isSignin, setIsSignin] = useState<boolean>(true);
 
-  const onSignIn = useCallback(async (data: FormData) => {
-    try {
-      const createdUser = await Database.createUser(data.email, data.password, data.name, data.isAuthor);
-    } catch (err: any) {
-      if(err.response.status === 401) {
-     
-      }
-      else if(err.response.status === 0) {
-   
-      }
-      else {
-   
-      }; 
-    }
+  const methods: UseFormReturn = useForm({
+    resolver: zodResolver(validationSchema),
+  });
+  const { control, handleSubmit, formState: { errors } } = methods;
+
+  const onSignIn = useCallback(async (data: any) => {
+    // if(data.password !== data.repeatPassword) control.setError('repeatPassword', {message: 'uuuuu'});
+    console.log(data)
+    // try {
+    //   const createdUser = await Database.createUser(data.email, data.password, data.name, data.isAuthor);
+    // } catch (err: any) {
+    //   if (err.response.status === 401) {
+
+    //   }
+    //   else if (err.response.status === 0) {
+
+    //   }
+    //   else {
+
+    //   };
+    // }
   }, [])
-  
-  const onSignUp = useCallback(async (data: FormData) => {
-    try {
-      const createdUser = await Database.createUser(data.email, data.password, data.name, data.isAuthor);
-    } catch (err: any) {
-      if(err.response.status === 401) {
-     
-      }
-      else if(err.response.status === 0) {
-   
-      }
-      else {
-   
-      }; 
-    }
+
+  const onSignUp = useCallback(async (data: any) => {
+    console.log('Signup')
+    // try {
+    //   const user = await Database.signin(data.email, data.password);
+    // } catch (err: any) {
+    //   if (err.response.status === 401) {
+
+    //   }
+    //   else if (err.response.status === 0) {
+
+    //   }
+    //   else {
+
+    //   };
+    // }
   }, [])
 
   return (
-    <AuthPageLayout />
+    // <AuthPageLayout methods={methods} />
+    <AuthPageLayout onSubmit={isSignin ? onSignIn : onSignUp} methods={methods} />
+    // <AuthPageLayout onSubmit={isSignin ? onSignIn : onSignUp} control={control} formErrors={errors} />
   )
 };
 
--- src/pages/AuthPage/AuthPageLayout.tsx
+++ src/pages/AuthPage/AuthPageLayout.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Button, Card, Container, FilledInput, IconButton, InputAdornment, InputLabel, SxProps, TextField } from "@mui/material/";
-import { Controller, FieldValues, FormProvider, useForm } from "react-hook-form";
+import { Control, Controller, FieldValues, FormProvider, UseFormReturn, useForm } from "react-hook-form";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 
@@ -9,22 +9,36 @@ const classes: { [key: string]: SxProps } = {
   card: { display: 'flex', flexDirection: 'column', width: 500, pl: 4, pr: 4, height: 500 }
 }
 
-const AuthPageLayout: FC = () => {
-  const methods = useForm();
+type AuthT = {
+  onSubmit: (data: any) => void;
+  // control: Control<FieldValues, any>;
+  // // handleSubmit: (data: FieldValues) => void;
+  // formErrors: {
+  //   [x: string]: any;
+  // };
+  methods: UseFormReturn;
+}
+
+const AuthPageLayout: FC<AuthT> = ({ onSubmit, methods }) => {
+  // const methods = useForm();
   const [showPassword, setShowPassword] = React.useState(false);
-  const { control, handleSubmit } = methods;
+  const { control, handleSubmit, formState: { errors } } = methods;
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    repeatPassword: ''
+  }); // to avoid react swear on none control
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
 
-  const onSave = (data: FieldValues) => {
-    console.log(data);
-  }
   return (
     <Container sx={{ height: '100vh', display: 'flex', alignContent: 'center', justifyContent: 'center' }}>
-      <FormProvider {...methods}>
+      {/* <FormProvider {...methods}> */}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Card sx={classes.card}>
           <h1 className="title">Sign Up</h1>
           <Controller
@@ -34,13 +48,19 @@ const AuthPageLayout: FC = () => {
               <TextField
                 sx={classes.input}
                 required
-                value={value}
-                onChange={onChange}
+                value={values.name}
+                onChange={(e) => {
+                  setValues((currentState) => ({ ...currentState, name: e.target.value }));
+                  onChange(e);
+                }}
                 variant="filled"
                 label="Name"
               />
             )}
           />
+          {errors.name && (
+            <span>{errors.name.message as string}</span>
+          )}
           <Controller
             name="email"
             control={control}
@@ -49,50 +69,31 @@ const AuthPageLayout: FC = () => {
                 sx={classes.input}
                 type="email"
                 required
-                value={value}
-                onChange={onChange}
+                value={values.email}
+                onChange={(e) => {
+                  setValues((currentState) => ({ ...currentState, email: e.target.value }));
+                  onChange(e);
+                }}
                 variant="filled"
                 label="Email"
               />
             )}
           />
+          {errors.email && (
+            <span>{errors.email.message as string}</span>
+          )}
           <Controller
             name="password"
-            control={control}
-            render={({ field: { value, onChange } }) => (
-              <>
-                <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
-                <FilledInput
-                  sx={classes.input}
-                  required
-                  value={value}
-                  onChange={onChange}
-                  type={showPassword ? 'text' : 'password'}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </>
-            )}
-          />
-          <Controller
-            name="repeatPassword"
             control={control}
             render={({ field: { value, onChange } }) => (
               <FilledInput
                 sx={classes.input}
                 required
-                value={value}
-                onChange={onChange}
+                value={values.password}
+                onChange={(e) => {
+                  setValues((currentState) => ({ ...currentState, password: e.target.value }));
+                  onChange(e);
+                }}
                 type={showPassword ? 'text' : 'password'}
                 endAdornment={
                   <InputAdornment position="end">
@@ -109,9 +110,44 @@ const AuthPageLayout: FC = () => {
               />
             )}
           />
-          <Button onClick={handleSubmit(onSave)}>Сохранить</Button>
+          {errors.password && (
+            <span>{errors.password.message as string}</span>
+          )}
+          <Controller
+            name="repeatPassword"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <FilledInput
+                sx={classes.input}
+                required
+                value={values.repeatPassword}
+                onChange={(e) => {
+                  setValues((currentState) => ({ ...currentState, repeatPassword: e.target.value }));
+                  onChange(e);
+                }}
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            )}
+          />
+          {errors.repeatPassword && (
+            <span>{errors.repeatPassword.message as string}</span>
+          )}
+          <Button type="submit">Submit</Button>
         </Card>
-      </FormProvider>
+      </form>
+      {/* </FormProvider> */}
     </Container>
   )
 };
